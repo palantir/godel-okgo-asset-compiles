5db9335d2ae270f135f24ef6b0053e0bf42f4e76
Nick Miyake <nmiyake@palantir.com>
2018-03-21 10:16:44 -0700

    Implement configuration upgrade logic

--- integration_test/integration_test.go
+++ integration_test/integration_test.go
@@ -18,13 +18,14 @@ import (
 	"testing"
 
 	"github.com/nmiyake/pkg/gofiles"
+	"github.com/palantir/godel/framework/pluginapitester"
 	"github.com/palantir/godel/pkg/products"
 	"github.com/palantir/okgo/okgotester"
 	"github.com/stretchr/testify/require"
 )
 
 const (
-	okgoPluginLocator  = "com.palantir.okgo:okgo-plugin:0.2.0"
+	okgoPluginLocator  = "com.palantir.okgo:check-plugin:1.0.0-rc3"
 	okgoPluginResolver = "https://palantir.bintray.com/releases/{{GroupPath}}/{{Product}}/{{Version}}/{{Product}}-{{Version}}-{{OS}}-{{Arch}}.tgz"
 
 	godelYML = `exclude:
@@ -36,18 +37,23 @@ const (
 `
 )
 
-func TestCompiles(t *testing.T) {
+func TestCheck(t *testing.T) {
 	assetPath, err := products.Bin("compiles-asset")
 	require.NoError(t, err)
 
 	configFiles := map[string]string{
-		"godel/config/godel.yml": godelYML,
-		"godel/config/check.yml": "",
+		"godel/config/godel.yml":        godelYML,
+		"godel/config/check-plugin.yml": "",
 	}
 
+	pluginProvider, err := pluginapitester.NewPluginProviderFromLocator(okgoPluginLocator, okgoPluginResolver)
+	require.NoError(t, err)
+
 	okgotester.RunAssetCheckTest(t,
-		okgoPluginLocator, okgoPluginResolver,
-		assetPath, "compiles",
+		pluginProvider,
+		pluginapitester.NewAssetProvider(assetPath),
+		"compiles",
+		"",
 		[]okgotester.AssetTestCase{
 			{
 				Name: "compile error in file",
@@ -62,6 +68,7 @@ func TestCompiles(t *testing.T) {
 				WantOutput: `Running compiles...
 foo.go:1:14: expected declaration, found 'IDENT' foo
 Finished compiles
+Check(s) produced output: [compiles]
 `,
 			},
 			{
@@ -81,6 +88,7 @@ Finished compiles
 				WantOutput: `Running compiles...
 ../foo.go:1:14: expected declaration, found 'IDENT' foo
 Finished compiles
+Check(s) produced output: [compiles]
 `,
 			},
 			{
@@ -100,7 +108,150 @@ Finished compiles
 				WantOutput: `Running compiles...
 foo_test.go:1:19: expected declaration, found 'IDENT' foo
 Finished compiles
+Check(s) produced output: [compiles]
 `,
+			},
+			{
+				Name: "non-empty configuration is not allowed",
+				ConfigFiles: map[string]string{
+					"godel/config/godel.yml": godelYML,
+					"godel/config/check-plugin.yml": `
+checks:
+  compiles:
+    config:
+      key: value
+`,
+				},
+				WantError: true,
+				WantOutput: `Error: failed to upgrade check "compiles" configuration: failed to upgrade asset configuration: compiles-asset does not currently support configuration
+`,
+			},
+		},
+	)
+}
+
+func TestUpgradeConfig(t *testing.T) {
+	pluginProvider, err := pluginapitester.NewPluginProviderFromLocator(okgoPluginLocator, okgoPluginResolver)
+	require.NoError(t, err)
+
+	assetPath, err := products.Bin("compiles-asset")
+	require.NoError(t, err)
+	assetProvider := pluginapitester.NewAssetProvider(assetPath)
+
+	pluginapitester.RunUpgradeConfigTest(t,
+		pluginProvider,
+		[]pluginapitester.AssetProvider{assetProvider},
+		[]pluginapitester.UpgradeConfigTestCase{
+			{
+				Name: `legacy configuration with empty "args" field is updated`,
+				ConfigFiles: map[string]string{
+					"godel/config/godel.yml": godelYML,
+					"godel/config/check-plugin.yml": `
+legacy-config: true
+checks:
+  compiles:
+    filters:
+      - value: "should have comment or be unexported"
+      - type: name
+        value: ".*.pb.go"
+`,
+				},
+				WantOutput: `Upgraded configuration for check-plugin.yml
+`,
+				WantFiles: map[string]string{
+					"godel/config/check-plugin.yml": `release-tag: ""
+checks:
+  compiles:
+    skip: false
+    priority: null
+    config: {}
+    filters:
+    - type: ""
+      value: should have comment or be unexported
+    exclude:
+      names:
+      - .*.pb.go
+      paths: []
+exclude:
+  names: []
+  paths: []
+`,
+				},
+			},
+			{
+				Name: `legacy configuration with non-empty "args" field fails`,
+				ConfigFiles: map[string]string{
+					"godel/config/godel.yml": godelYML,
+					"godel/config/check-plugin.yml": `
+legacy-config: true
+checks:
+  compiles:
+    args:
+      - "-foo"
+`,
+				},
+				WantError: true,
+				WantOutput: `Failed to upgrade configuration:
+	godel/config/check-plugin.yml: failed to upgrade check "compiles" legacy configuration: failed to upgrade asset configuration: compiles-asset does not support legacy configuration with a non-empty "args" field
+`,
+				WantFiles: map[string]string{
+					"godel/config/check-plugin.yml": `
+legacy-config: true
+checks:
+  compiles:
+    args:
+      - "-foo"
+`,
+				},
+			},
+			{
+				Name: `empty v0 config works`,
+				ConfigFiles: map[string]string{
+					"godel/config/godel.yml": godelYML,
+					"godel/config/check-plugin.yml": `
+checks:
+  compiles:
+    skip: true
+    # comment preserved
+    config:
+`,
+				},
+				WantOutput: ``,
+				WantFiles: map[string]string{
+					"godel/config/check-plugin.yml": `
+checks:
+  compiles:
+    skip: true
+    # comment preserved
+    config:
+`,
+				},
+			},
+			{
+				Name: `non-empty v0 config does not work`,
+				ConfigFiles: map[string]string{
+					"godel/config/godel.yml": godelYML,
+					"godel/config/check-plugin.yml": `
+checks:
+  compiles:
+    config:
+      # comment
+      key: value
+`,
+				},
+				WantError: true,
+				WantOutput: `Failed to upgrade configuration:
+	godel/config/check-plugin.yml: failed to upgrade check "compiles" configuration: failed to upgrade asset configuration: compiles-asset does not currently support configuration
+`,
+				WantFiles: map[string]string{
+					"godel/config/check-plugin.yml": `
+checks:
+  compiles:
+    config:
+      # comment
+      key: value
+`,
+				},
 			},
 		},
 	)
