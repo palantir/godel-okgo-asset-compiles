732a9369af43dcde5924bbd24fcd419a11784a05
Nick Miyake <nmiyake@palantir.com>
2018-09-19 11:45:41 -0700

    Revert "Adjust checker based on Go runtime version (#23)"
    This reverts commit 314a251f3b5bd98e841024dc169ff2d3f549ea7e.

--- compiles/creator/creator.go
+++ compiles/creator/creator.go
@@ -21,10 +21,6 @@ import (
 	"github.com/palantir/godel-okgo-asset-compiles/compiles"
 )
 
-func init() {
-	checker.SetGoBuildDefaultReleaseTags()
-}
-
 func Compiles() checker.Creator {
 	return checker.NewCreator(
 		compiles.TypeName,
